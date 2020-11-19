645d9d7d197e9b1b856ba940daaeba75d516e93d
AKANE_CHAN <taiseinanai@gmail.com>
2020-11-19 11:12:37 +0900

    プレイヤーを追いかけるエネミーVer0.8ぐらい

---
+++ Assets/Nanai/Script/EnemyChaseMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyChaseMove : MonoBehaviour
+{
+    NavMeshAgent Nav;
+    GameObject Destination;
+    float NavDefaultSpeed;//初期移動速度保存用
+    [SerializeField] float Spd; //移動速度変更後に使用するSpeed
+    [SerializeField] float SensingRange; //感知範囲
+    [SerializeField] float SpdChangeRange; //感知後移動速度を変える距離
+    float Distance; //エネミーとPlayerの距離
+    void Start()
+    {
+        //エネミーのNavMeshAgentを取得
+        Nav = GetComponent<NavMeshAgent>();
+        //目的地のオブジェクトを取得
+        Destination = GameObject.Find("Player");
+        NavDefaultSpeed = Nav.speed;
+    }
+
+    void Update()
+    {
+        //2点間の距離を計測
+        Distance = Vector3.Distance(Destination.transform.position, this.transform.position);
+        if(Distance <= SensingRange)
+        {
+            //目的地を設定
+            Nav.SetDestination(Destination.transform.position);
+            
+            if(Distance <= SpdChangeRange)
+            {
+                //移動速度変更
+                Nav.speed = Spd;
+            }
+            else
+            {
+                //移動速度を戻す
+                Nav.speed = NavDefaultSpeed;
+            }
+        }
+    }
+}
