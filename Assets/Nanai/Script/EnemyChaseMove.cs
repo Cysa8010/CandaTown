68a9fdcc4edffa5a21216272f2aa21895ae0288f
AKANE_CHAN <taiseinanai@gmail.com>
2020-12-04 15:38:28 +0900

    Enemyの巡回機能を実装 視界に入ったTargetがオブジェクトの影に隠れていた場合感知しないように変更

--- Assets/Nanai/Script/EnemyChaseMove.cs
+++ Assets/Nanai/Script/EnemyChaseMove.cs
@@ -5,54 +5,97 @@ using UnityEngine.AI;
 
 public class EnemyChaseMove : MonoBehaviour
 {
-    NavMeshAgent Nav;
-    [SerializeField] GameObject Destination; //目的地オブジェクト
-    float NavDefaultSpeed;//初期移動速度保存用
+    NavMeshAgent Agent;
+    Animator animator;
+    //以下主にTarget追跡の際に使用する変数
+    [SerializeField] GameObject Target; //最終的な目的地となるオブジェクト
+    float AgentDefaultSpeed;//初期移動速度保存用
     [SerializeField] float Spd; //移動速度変更後に使用するSpeed
     [SerializeField] float SensingRange; //感知範囲
     [SerializeField] float SpdChangeRange; //感知後移動速度を変える距離
-    float Distance; //エネミーと目的地オブジェクトの距離
-    [SerializeField] float searchAngle; //エネミーの視野(50と入力すると片側50度なので視界は100度になる)
-    Animator animator;
+    [SerializeField] float searchAngle; //Agentの視野(50と入力すると片側50度なので視界は100度になる)
+    float Distance; //Agentと目的地オブジェクトの距離
+    bool IsDiscovery;//Targetを発見したか否かのフラグ
+
+    //以下主に巡回の際に使用する変数
+    [SerializeField] Transform[] PatrolPoints;//巡回地点オブジェクトを格納する配列
+    int DestObjects = 0;//巡回地点のオブジェクト数（初期値=0）
     void Start()
     {
-        //エネミーのNavMeshAgentを取得
-        Nav = GetComponent<NavMeshAgent>();
-        //目的地のオブジェクトを取得
-        //fix [SerializeField]付けたのでInspectorからGameObjectを指定してください。これ多分重いから。
-        //Destination = GameObject.Find("Goal");
+        //AgentのNavMeshAgentを取得
+        Agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
-        NavDefaultSpeed = Nav.speed;
-
+        AgentDefaultSpeed = Agent.speed;
+        IsDiscovery = false;
+        //巡回地点間の移動を継続させるために自動ブレーキを無効化
+        Agent.autoBraking = false;
         //目的地を初期化
-        Nav.SetDestination(this.transform.position);
+        GotoNextPoint();
+        animator.SetBool("Is_Running", true);//仮置き
     }
 
     void Update()
     {
         //2点間の距離を計測
-        Distance = Vector3.Distance(Destination.transform.position, this.transform.position);
-        if(Distance <= SensingRange)
+        Distance = Vector3.Distance(Target.transform.position, this.transform.position);
+        if (IsDiscovery)
         {
-            Vector3 dist = Destination.transform.position - this.transform.position;
-            float angle = Vector3.Angle(transform.forward, dist);  // 敵から見たプレイヤーの方向
-            if (angle <= searchAngle)
-            {
-                animator.SetBool("Is_Running", true);
-                //目的地を設定
-                Nav.SetDestination(Destination.transform.position);
+            //目的地を設定
+            Agent.SetDestination(Target.transform.position);
 
-                if (Distance <= SpdChangeRange)
+            if (Distance <= SpdChangeRange)
+            {
+                //移動速度変更
+                Agent.speed = Spd;
+            }
+            else
+            {
+                //移動速度を戻す
+                Agent.speed = AgentDefaultSpeed;
+            }
+        }//IsDiscovery True
+        else
+        {
+            //TargetとAgentの位置から向きベクトルを作成
+            Vector3 dist = (Target.transform.position - this.transform.position).normalized;
+            //Agentから見たTargetの方向をfloat値にする
+            float angle = Vector3.Angle(transform.forward, dist);
+            //TargetがAgentの視界内かつ感知範囲内か判断
+            if (angle <= searchAngle && Distance <= SensingRange)
+            {
+                //rayにAgent座標とTargetの方向を格納
+                Ray ray = new Ray(this.transform.position, dist);
+                RaycastHit rayHit;
+
+                //AgentからTargetへ向けて感知範囲の距離だけRayを飛ばす 結果をrayHitに格納
+                Physics.Raycast(ray, out rayHit, SensingRange);
+                //Debug用Ray表示
+                Debug.DrawRay(ray.origin, ray.direction * SensingRange, Color.red, 1, false);
+                if (rayHit.collider.tag == "Player")
                 {
-                    //移動速度変更
-                    Nav.speed = Spd;
-                }
-                else
-                {
-                    //移動速度を戻す
-                    Nav.speed = NavDefaultSpeed;
+                    Debug.Log("Target発見");
+                    IsDiscovery = true;
                 }
             }
+
+            //Agentが現在の巡回地点に到達したら
+            if (!Agent.pathPending && Agent.remainingDistance < 0.5f)
+            {
+                //次の巡回地点を設定する
+                GotoNextPoint();
+            }
+        }//IsDiscovery false
+    }//Update
+    void GotoNextPoint()
+    {
+        //巡回地点が設定されていなければリターン
+        if (PatrolPoints.Length == 0)
+        {
+            return;
         }
+        // 現在選択されている配列の座標を巡回地点の座標に代入
+        Agent.SetDestination(PatrolPoints[DestObjects].position);
+        // 配列の中から次の巡回地点を選択（必要に応じて繰り返し）
+        DestObjects = (DestObjects + 1) % PatrolPoints.Length;
     }
 }
