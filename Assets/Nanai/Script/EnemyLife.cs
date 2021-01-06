aeec31a1d02d3868679ff91190fe7cc795e506a8
AKANE_CHAN <taiseinanai@gmail.com>
2021-01-06 23:41:38 +0900

    EnemyAのAnimationとか

---
+++ Assets/Nanai/Script/EnemyLife.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLife : MonoBehaviour
+{
+    GameObject GameSceneManagerObject;
+    GameSceneManager ManagerScript;
+    [SerializeField] int Life;
+    [SerializeField] int Damage;
+    [SerializeField] GameObject particleObject;
+
+    void Start()
+    {
+        GameSceneManagerObject = GameObject.Find("SceneManager");
+        ManagerScript = GameSceneManagerObject.GetComponent<GameSceneManager>();
+    }
+    void Update()
+    {
+        if (Life <= 0)
+        {
+            //if (true)
+            {
+                Instantiate(particleObject, this.transform.position, Quaternion.Euler(-90f,0f,0f));
+                ManagerScript.AddCount();
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "PlayerBullet")
+        {
+            Life -= Damage;
+        }
+    }
+}
---
+++ Assets/Nanai/Script/GameSceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameSceneManager : MonoBehaviour
+{
+    int i;
+    [SerializeField] int EnemyNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (i >= EnemyNum)
+        {
+            Debug.Log("GameClear,SceneChange");
+            //SceneManager.LoadScene("Scene_Game", LoadSceneMode.Single);
+        }
+    }
+    public void AddCount()
+    {
+        i += 1;
+    }
+}
